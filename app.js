3cbfa4389c092425144c186d092ea32c8a38285c
Adrian <adrian.asencio92@hotmail.com>
2021-03-12 13:33:13 +0100

    Nuevos cambios del Marcador

--- app.js
+++ app.js
@@ -1,59 +1,129 @@
-function jugar(){
+function jugar() {
     let piedra = document.getElementById('piedra');
-      piedra = 2;
+    piedra = 2;
     let papel = document.getElementById('papel');
-      papel = 1;
+    papel = 1;
     let tijera = document.getElementById('tijera');
-      tijera = 3;
+    tijera = 3;
     let numeroAleatorio = Math.round((Math.random() * 3));
-    
+    let resultado = document.getElementById('resultado-ganador');
+    resultado = 0;
+
     if (piedra < numeroAleatorio) {
-            document.getElementById('resultado').innerHTML = `Has ganado, elegiste Piedra y tu contrincante Tijeras`
-        } else if (piedra == numeroAleatorio) {
-            document.getElementById('resultado').innerHTML = `Has empatado, elegiste Piedra y tu contrincante tambien`
-        } else if (piedra > numeroAleatorio) {
-            document.getElementById('resultado').innerHTML = `Has perdido, elegiste Piedra y tu contrincante papel`
-        } else {
-            document.getElementById('resultado').innerHTML = "Houston tenemos un problema"
-        }
+        document.getElementById('resultado').innerHTML = `Has ganado, elegiste Piedra y tu contrincante Tijeras`;
+    } else if (piedra == numeroAleatorio) {
+        document.getElementById('resultado').innerHTML = `Has empatado, elegiste Piedra y tu contrincante tambien`
+    } else if (piedra > numeroAleatorio) {
+        document.getElementById('resultado').innerHTML = `Has perdido, elegiste Piedra y tu contrincante papel`
+    } else {
+        document.getElementById('resultado').innerHTML = "Houston tenemos un problema"
     }
-    
-    function jugar2(){
+}
+
+function jugar2() {
     let piedra = document.getElementById('piedra');
-      piedra = 1;
+    piedra = 1;
     let papel = document.getElementById('papel');
-      papel = 2;
+    papel = 2;
     let tijera = document.getElementById('tijera');
-      tijera = 3;
+    tijera = 3;
     let numeroAleatorio = Math.round((Math.random() * 3));
-    
+
     if (papel < numeroAleatorio) {
-            document.getElementById('resultado').innerHTML = `Has perdido, elegiste papel y tu contrincante Tijeras`
-        } else if (papel == numeroAleatorio) {
-            document.getElementById('resultado').innerHTML = `Has empatado, elegiste Papel y tu contrincante tambien`
-        } else if (papel > numeroAleatorio) {
-            document.getElementById('resultado').innerHTML = `Has ganado, elegiste Papel y tu contrincante Piedra`
-        } else {
-            document.getElementById('resultado').innerHTML = "Houston tenemos un problema"
-        }
+        document.getElementById('resultado').innerHTML = `Has perdido, elegiste papel y tu contrincante Tijeras`
+    } else if (papel == numeroAleatorio) {
+        document.getElementById('resultado').innerHTML = `Has empatado, elegiste Papel y tu contrincante tambien`
+    } else if (papel > numeroAleatorio) {
+        document.getElementById('resultado').innerHTML = `Has ganado, elegiste Papel y tu contrincante Piedra`
+    } else {
+        document.getElementById('resultado').innerHTML = "Houston tenemos un problema"
     }
-    
-    function jugar3(){
+}
+
+function jugar3() {
     let piedra = document.getElementById('piedra');
-      piedra = 3;
+    piedra = 3;
     let papel = document.getElementById('papel');
-      papel = 1;
+    papel = 1;
     let tijera = document.getElementById('tijera');
-      tijera = 2;
+    tijera = 2;
     let numeroAleatorio = Math.round((Math.random() * 3));
-    
+
     if (tijera < numeroAleatorio) {
-            document.getElementById('resultado').innerHTML = `Has perdido, elegiste Tijera y tu contrincante Piedra`
-        } else if (tijera == numeroAleatorio) {
-            document.getElementById('resultado').innerHTML = `Has empatado, elegiste Tijera y tu contrincante tambien`
-        } else if (tijera > numeroAleatorio) {
-            document.getElementById('resultado').innerHTML = `Has ganado, elegiste Tijera y tu contrincante Papel`
-        } else {
-            document.getElementById('resultado').innerHTML = "Houston tenemos un problema"
-        }
+        document.getElementById('resultado').innerHTML = `Has perdido, elegiste Tijera y tu contrincante Piedra`
+    } else if (tijera == numeroAleatorio) {
+        document.getElementById('resultado').innerHTML = `Has empatado, elegiste Tijera y tu contrincante tambien`
+    } else if (tijera > numeroAleatorio) {
+        document.getElementById('resultado').innerHTML = `Has ganado, elegiste Tijera y tu contrincante Papel`
+    } else {
+        document.getElementById('resultado').innerHTML = "Houston tenemos un problema"
     }
+}
+
+
+
+
+
+
+
+
+/*function jugar() {
+  let piedra = document.getElementById('piedra');
+  piedra = 2;
+  let papel = document.getElementById('papel');
+  papel = 1;
+  let tijera = document.getElementById('tijera');
+  tijera = 3;
+  let numeroAleatorio = Math.round((Math.random() * 3));
+  let resultado = 0;
+
+  if (piedra < numeroAleatorio) {
+      document.getElementById('resultado').innerHTML = `Has ganado, elegiste Piedra y tu contrincante Tijeras`
+  } else if (piedra == numeroAleatorio) {
+      document.getElementById('resultado').innerHTML = `Has empatado, elegiste Piedra y tu contrincante tambien`
+  } else if (piedra > numeroAleatorio) {
+      document.getElementById('resultado').innerHTML = `Has perdido, elegiste Piedra y tu contrincante papel`
+  } else {
+      document.getElementById('resultado').innerHTML = "Houston tenemos un problema"
+  }
+}
+
+function jugar2() {
+  let piedra = document.getElementById('piedra');
+  piedra = 1;
+  let papel = document.getElementById('papel');
+  papel = 2;
+  let tijera = document.getElementById('tijera');
+  tijera = 3;
+  let numeroAleatorio = Math.round((Math.random() * 3));
+
+  if (papel < numeroAleatorio) {
+      document.getElementById('resultado').innerHTML = `Has perdido, elegiste papel y tu contrincante Tijeras`
+  } else if (papel == numeroAleatorio) {
+      document.getElementById('resultado').innerHTML = `Has empatado, elegiste Papel y tu contrincante tambien`
+  } else if (papel > numeroAleatorio) {
+      document.getElementById('resultado').innerHTML = `Has ganado, elegiste Papel y tu contrincante Piedra`
+  } else {
+      document.getElementById('resultado').innerHTML = "Houston tenemos un problema"
+  }
+}
+
+function jugar3() {
+  let piedra = document.getElementById('piedra');
+  piedra = 3;
+  let papel = document.getElementById('papel');
+  papel = 1;
+  let tijera = document.getElementById('tijera');
+  tijera = 2;
+  let numeroAleatorio = Math.round((Math.random() * 3));
+
+  if (tijera < numeroAleatorio) {
+      document.getElementById('resultado').innerHTML = `Has perdido, elegiste Tijera y tu contrincante Piedra`
+  } else if (tijera == numeroAleatorio) {
+      document.getElementById('resultado').innerHTML = `Has empatado, elegiste Tijera y tu contrincante tambien`
+  } else if (tijera > numeroAleatorio) {
+      document.getElementById('resultado').innerHTML = `Has ganado, elegiste Tijera y tu contrincante Papel`
+  } else {
+      document.getElementById('resultado').innerHTML = "Houston tenemos un problema"
+  }
+}*/
